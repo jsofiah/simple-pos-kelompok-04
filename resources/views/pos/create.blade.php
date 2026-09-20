8d4a8242a8998081772d83e0c3f859017a9e6b5f
jsofiah <j.sofiah18@gmail.com>
2026-09-20 15:17:34 +0700

    tambah tombol hapus

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -10,6 +10,9 @@
         addToCart(id, name, price) {
             this.cart.push({ id, name, price });
         },
+        removeFromCart(id) {
+            this.cart = this.cart.filter(item => item.id !== id);
+        },
         subtotal() {
             return this.cart.reduce((sum, item) => sum + item.price, 0);
         }
@@ -25,8 +28,15 @@
         </div>
 
         <div class="mt-4 border-t pt-3">
-            <template x-for="item in cart" :key="item.id">
-                <p x-text="item.name + ' - Rp ' + item.price"></p>
+            <template x-for="(item, index) in cart" :key="index">
+                <div class="flex items-center justify-between py-1">
+                    <p x-text="item.name + ' - Rp ' + item.price"></p>
+                    <button type="button"
+                        class="text-xs text-red-600 hover:text-red-800 hover:underline"
+                        @click="removeFromCart(item.id)">
+                        Hapus
+                    </button>
+                </div>
             </template>
             <p class="font-semibold mt-2">
                 Subtotal: Rp <span x-text="subtotal()"></span>
